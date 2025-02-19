cc1b181de3c8da7082848f031e9e093681afa6df
Shadab_Alam <mdshadabalam7799@gmail.com>
2025-02-19 18:42:00 +0530

    added more test cases

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -1,11 +1,17 @@
 from src.math_operations import add,sub
 
+#Test case 1
 def test_add():
     assert add(2,3)==5
     assert add(-1,1)==0
-    
+
+#Test case 2
 def test_sub():
     assert sub(5,3)==2
     assert sub(4,3)==1
     assert sub(3,3)==0
     assert sub(2,3)==-1
+
+#Test case 3
+def test_custom():
+    assert add(2,3)+sub(5,3)==7
